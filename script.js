4ec7054184146c5975cca021eb9fba85bb408e29
jorjorb3 <32470446+jorjorb3@users.noreply.github.com>
2017-11-26 16:11:27 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+<!-- Carousel -->
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+
+<!-- Carousel Indicators -->
+<li data-target="#myCarousel" data-slide-to="1"></li>
+
+<!-- Carousel Controls -->
+<a class="left carousel-control" href="#myCarousel" data-slide="prev">

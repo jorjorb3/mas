652a37db5de510ee069163c6be63ce3dcb9b1e51
jorjorb3 <32470446+jorjorb3@users.noreply.github.com>
2017-12-04 13:21:00 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,66 @@
-<!-- Carousel -->
-<div id="myCarousel" class="carousel slide" data-ride="carousel">
+(function() {
 
-<!-- Carousel Indicators -->
-<li data-target="#myCarousel" data-slide-to="1"></li>
+	function Slideshow( element ) {
+		this.el = document.querySelector( element );
+		this.init();
+	}
 
-<!-- Carousel Controls -->
-<a class="left carousel-control" href="#myCarousel" data-slide="prev">
+	Slideshow.prototype = {
+		init: function() {
+			this.wrapper = this.el.querySelector( ".slider-wrapper" );
+			this.slides = this.el.querySelectorAll( ".slide" );
+			this.previous = this.el.querySelector( ".slider-previous" );
+			this.next = this.el.querySelector( ".slider-next" );
+			this.index = 0;
+			this.total = this.slides.length;
+			this.timer = null;
+
+			this.action();
+			this.stopStart();
+		},
+		_slideTo: function( slide ) {
+			var currentSlide = this.slides[slide];
+			currentSlide.style.opacity = 1;
+
+			for( var i = 0; i < this.slides.length; i++ ) {
+				var slide = this.slides[i];
+				if( slide !== currentSlide ) {
+					slide.style.opacity = 0;
+				}
+			}
+		},
+		action: function() {
+			var self = this;
+			self.timer = setInterval(function() {
+				self.index++;
+				if( self.index == self.slides.length ) {
+					self.index = 0;
+				}
+				self._slideTo( self.index );
+
+			}, 3000);
+		},
+		stopStart: function() {
+			var self = this;
+			self.el.addEventListener( "mouseover", function() {
+				clearInterval( self.timer );
+				self.timer = null;
+
+			}, false);
+			self.el.addEventListener( "mouseout", function() {
+				self.action();
+
+			}, false);
+		}
+
+
+	};
+
+	document.addEventListener( "DOMContentLoaded", function() {
+
+		var slider = new Slideshow( "#main-slider" );
+
+	});
+
+
+})();
